086560d03c541f9682650fc15f7086950dfef304
Pedro Rocha Martin <63104901+rochasdemarte@users.noreply.github.com>
2022-05-12 13:24:25 -0300

    Update hostedPlayer.js

--- hostedPlayer.js
+++ hostedPlayer.js
@@ -1,5 +1,5 @@
 /*
-This code is part of the Web performance: Hosted Player - Revisiting Olia Lialina's web performance for a swimmer "Hosted".
+This code is part of the Work: Hosted Player - Revisiting Olia Lialina's web performance for a swimmer "Hosted".
 A tribute to the Lialina's work available at: https://hosted.z21.web.core.windows.net/
 
 Try it yourself:
